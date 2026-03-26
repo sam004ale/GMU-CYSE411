78285bbd6d7d999156e11ed8d55ceba940d52540
sam004ale <154575955+sam004ale@users.noreply.github.com>
2026-03-25 22:55:06 -0400

    Update q1.js

--- extra-exe1/q1.js
+++ extra-exe1/q1.js
@@ -4,12 +4,11 @@
  * @param {string[]} notifications - Array of notification messages.
  */
 function renderNotifications(listEl, notifications) {
-  listEl.innerHTML = ""; // Clear list
+  listEl.innerHTML = "";
+
   notifications.forEach(msg => {
-    // VULNERABILITY: Using innerHTML allows DOM-based XSS
-    listEl.innerHTML += "<li>" + msg + "</li>";
+    const li = document.createElement("li");
+    li.textContent = msg;
+    listEl.appendChild(li);
   });
 }
-
-// Example Attack:
-// renderNotifications(el, ["System update", "<img src=x onerror=alert('XSS')>"]);
